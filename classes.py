9829fd190b6668dcbaa121a3f4b29433ba8b378f
León <leon.grass@gmx.net>
2021-09-10 22:58:11 +0200

    gute nacht schlafmütze
    Co-authored-by: Amelie-Auer <Amelie-Auer@users.noreply.github.com>

--- classes.py
+++ classes.py
@@ -1,9 +1,29 @@
 import pygame, sys, math
 
+
 class Figure:
-    def __init__(self, name:str, position:tuple):
+    def __init__(self, name:str,image_name:str, position:tuple, screen, rect_size):
         self.name = name
+        self.image_name = image_name
         self.position = position
+        self.screen = screen
+        self.rect_size = rect_size
+        self.chars = 'ABCDEFGH'
+        self.image = pygame.image.load(f"./figures/{self.image_name}.png") # loading the corresponding image
+        self.image.convert()
+        self.image = pygame.transform.rotozoom(self.image, 0, self.rect_size / self.image.get_height()) # scaling the image to the right size 
+        self.c_x, self.c_y = self.position[1]-1, self.chars.index(self.position[0]) # defining x and y coordinates
+
+    def place_figure(self):
+        self.screen.blit(self.image, (((self.rect_size/2) - self.image.get_width() / 2)+ self.c_x*self.rect_size, self.c_y*self.rect_size))
+
+    def move_figure(self, position):
+        old_position = self.position
+        self.position = position
+        self.c_x, self.c_y = self.position[1]-1, self.chars.index(self.position[0]) # defining x and y coordinates
+        self.place_figure()
+        #deleting old image here
+
 
         
 class Farmer(Figure):
--- game.py
+++ game.py
@@ -1,6 +1,6 @@
-import pygame, sys, math
+import pygame, sys, math, time
 from methods import *
-
+from classes import *
 pygame.init()
 
 # creating window
@@ -41,24 +41,40 @@ pygame.draw.rect(screen, side_color, (8*rect_size, 8*rect_size,chars_space_size,
 
 first_row_figures = {'tower':[0,7], 'horse':[1,6], 'runner':[2,5], 'lady':[4], 'king':[3]}
 # loop for both players
+
+players = {}
 for player in range(1,3):
-    #placing first row items 
+    #placing first row items
     for field in range(8):
         x = [(key if field in value else '') for key, value in first_row_figures.items()] # zb ['','','','tower', '']
         x.sort() # ['','',''..., 'tower']
         figure = x[-1]
-        place_figure(figure if player == 1 else figure + '_black',rect_size, screen, ('A' if player == 1 else 'H', field+1))
+        color = ('_black' if player == 1 else '_white')
+        name = figure + ("_2" if (figure+'_1' + color) in list(players.keys()) else '_1') + color
+        image_name = figure + ('_black' if player == 1 else '')
+        place = ('A' if player == 1 else 'H', field+1)
+        players[name] = Figure(name, image_name,place, screen, rect_size)
 
     #mathematical impovements needed ?!
     for field in range(8,16): # placing farmers in second row
         #placing of the farmers
         place = list(board_data.keys())[field] if player == 1 else list(board_data.keys())[-(field+1)]
-        place_figure('farmer' if player == 1 else 'farmer_black',rect_size, screen, place)
+        name = 'farmer_' + str(8-field%8) + ('_black' if player == 1 else '_white')
+        image_name = 'farmer' + ('_black' if player == 1 else '')
+        players[name] = Figure(name,image_name, place, screen, rect_size)
 
+
+
+for name, object_ in players.items():
+    print(name)
+    object_.place_figure()
 go = True
 while go:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+    pressed = pygame.key.get_pressed()
+    # if pressed[pygame.K_UP]:
+    #     players['farmer_1_white'].move_figure((players['farmer_1_white'].chars[players['farmer_1_white'].chars.index(players['farmer_1_white'].position[0])-1], 1))
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(30)
